efbce2169c14f90d0f628070d4a9f57d18d01d0c
alemantus <Alexander@anak.dk>
2019-01-21 12:11:12 +0100

    Done stringing

--- LindIter.m
+++ LindIter.m
@@ -1,1 +1,25 @@
 function LindenmayerString = LindIter(System, N)
+
+if strcmp(System, "Koch")
+    ourStr = "S";
+    for i = 1:N
+        ourStr = strrep(ourStr, "S", "SLR");
+    end    
+    
+elseif strcmp(System, "Sierpinski")
+    
+    ourStr = "A";
+    
+    for k = 1:N
+        old = {'A','B'};
+        new = {'BRARB','ALBLA'};
+        ourStr = replace(ourStr,old,new);  
+    end
+
+end
+
+
+
+LindenmayerString = ourStr;
+
+disp(LindenmayerString);
