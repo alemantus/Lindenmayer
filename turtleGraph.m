8d29050913303143be682e03468a006910d0f490
alemantus <Alexander@anak.dk>
2019-01-22 16:50:40 +0100

    nu

--- turtleGraph.m
+++ turtleGraph.m
@@ -39,8 +39,8 @@ elseif any(coord=='[')
     coord(coord=='1')=(1/2);
     coord(coord=='0')=(1/4);
     
-    coord(coord==']') =-1/4*pi;
-    coord(coord=='[') =1/4*pi;  
+    coord(coord==']') =-0.7854;
+    coord(coord=='[') =0.7854;  
     type = 2;
     disp(coord);
 end
