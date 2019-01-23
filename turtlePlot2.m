d32f6b7f305d57130c77934927647a247896e864
Gostraf <40057775+Gostraf@users.noreply.github.com>
2019-01-23 22:51:45 +0100

    Update turtlePlot2.m

--- turtlePlot2.m
+++ turtlePlot2.m
@@ -2,67 +2,8 @@ function turtlePlot2(turtleCommands)
 
 d=[[1;0] zeros(2,length(turtleCommands/2))];
 x=[[0;0] zeros(2,length(turtleCommands/2))];
-angle = 0;
-stackAng=0;
-stackPos=[0;0];
-
-
-stackDex = 1;
-
-%since every len entry is the same
-lineLen = turtleCommands(1);
-%extracts every even index entry from turtleCommands
-lineAng = turtleCommands(2:2:end);
-%The first values needs to be initialized manually
-d=[[1;0] zeros(2,length(lineAng)-1)];
-x=[[0;0], zeros(2,length(lineAng)-1)];
 
 for i = 1:length(turtleCommands)
-<<<<<<< HEAD
-    if turtleCommands(i)==1/4*pi       
-        %disp(angle);
-        stackAng=[stackAng;angle]; %Push angle values to stack        
-        stackPos=[stackPos, d(:,i)]; %Push pos values to stack
-        angle=angle+pi/4;   
-        d(:,i+1)=[cos(angle), -sin(angle); sin(angle), cos(angle)]*d(:,i);
-        
-
-    elseif turtleCommands(i)==(-1/4*pi)
-
-        d(:,i)=stackPos(:,end);
-        
-        angle=stackAng(end);
-        
-        
-        stackDex = stackDex-1;
-        
-        angle=angle-pi/4;
-        d(:,i+1)=[cos(angle), -sin(angle); sin(angle), cos(angle)]*d(:,i);
-        stackAng(end)=[]; %Pop X values from stack
-        stackPos(:,end)=[]; %Pop D values from stack
-        
-        disp(i);
-        disp(angle);
-        
-        
-    else
-        d(:,i+1)=d(:,i);        
-    end
-      %disp(angle);  
-    if turtleCommands(i)==1/2
-        x(:,i+1)=x(:,i)+1/2*d(:,i+1);    
-        %disp("test1");
-    elseif turtleCommands(i)==1/4
-        %disp("test2");
-        x(:,i+1)=x(:,i)+1/4*d(:,i+1);
-    end
-
-
-end
-%        disp(d);
-%        disp(x);
-plot(x(1,:),x(2,:));
-=======
     if turtleCommands(2,i) == 2 %Checks if current command is an angle
         d(:,i+1)=[cos(turtleCommands(1, i)), -sin(turtleCommands(1, i)); sin(turtleCommands(1, i)), cos(turtleCommands(1, i))]*d(:,i);
         x(:,i+1)=x(:,i); %Next position will be the same
@@ -86,5 +27,4 @@ plot(x(1,:),x(2,:));
 
 end
 
-%line(x(1,:), x(2,:));
->>>>>>> Gustav
+line(x(1,:), x(2,:));
