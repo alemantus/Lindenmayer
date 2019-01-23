c2447d2bd879ce2378da094d37f69f86c2159d3d
Gostraf <40057775+Gostraf@users.noreply.github.com>
2019-01-23 22:36:44 +0100

    Graph and plot fixed

--- turtleGraph.m
+++ turtleGraph.m
@@ -1,7 +1,7 @@
 function [turtleCommands] = turtleGraph(LindenmayerString)
 
+%disp(LindenmayerString);
 coord = double(cell2mat(split(LindenmayerString,""))).';
-
 
 %Koch
 if any(coord=='S')
@@ -32,6 +32,22 @@ elseif any(coord=='A')
     
     type = 1;
     
+elseif any(coord=='F')
+    
+    %Number of iterations specified in LindIter.m
+    iteration = log(numel(coord)/2+1/2)/log(3);
+    
+    %length determination
+    coord(coord=='F')=(1/2)^iteration;
+    %coord(coord=='F')=[];
+    
+    %the angle depends on the first letter
+    coord(coord=='R') =-(1/2)*pi;
+    coord(coord=='L') =(1/2)*pi;
+    
+    %disp(coord);
+    
+    
 elseif any(coord=='[')
     
     %iteration = log(numel(coord)/2+1/2)/log(3);
@@ -42,7 +58,11 @@ elseif any(coord=='[')
     coord(coord==']') =-1/4*pi;
     coord(coord=='[') =1/4*pi;  
     type = 2;
+<<<<<<< HEAD
 %     disp(coord);
+=======
+    %disp(coord);
+>>>>>>> Gustav
 end
 
 turtleCommands = coord;
